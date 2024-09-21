89bb3c0ae31a21453a2ee697a69737d131d565fe
Apprentice-seth <2965188055@qq.com>
2024-09-21 09:47:35 +0800

    Add files via upload

---
+++ predicter.py
@@ -0,0 +1,55 @@
+# import numpy as np
+import torch
+import torchvision.models as models
+import torchvision.transforms as transforms
+# from matplotlib import pyplot as plt
+from torchvision.datasets import CIFAR10
+from torch.utils.data import DataLoader
+
+
+Net = models.resnet50()
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+Net.to(device)
+Net.eval()
+transform = transforms.Compose([
+    transforms.Resize((224, 224)),
+    transforms.ToTensor(),
+    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+])
+
+train_set = CIFAR10(root="", train=True, transform=transform, download=False)
+test_set = CIFAR10(root="", train=False, transform=transform, download=False)
+train_dataloader = DataLoader(train_set, batch_size=32, shuffle=True)
+test_dataloader = DataLoader(train_set, batch_size=32, shuffle=True)
+
+
+def evaluate(test_data, net):
+    n_correct = 0
+    n_total = 0
+    with torch.no_grad():
+        for (x, y) in test_data:
+            x = x.to(device)
+            y = y.to(device)
+            outputs = net(x)
+            preds = torch.argmax(outputs, dim=1)
+            n_correct += (preds == y).sum().item()
+            n_total += y.size(0)
+    return n_correct / n_total
+
+
+Net.load_state_dict(torch.load('model/model30.pt'))
+class_names = ['airplane', 'automobile', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck']
+print("model accuracy:", evaluate(test_dataloader, Net))
+
+# for (n, (x, _)) in enumerate(test_dataloader):
+#     if n > 9:
+#         break
+#     x = x.to(device)
+#     predict = torch.argmax(Net(x), dim=1)
+#     plt.figure(n)
+#     img = x[0].permute(1, 2, 0).cpu().numpy()
+#     img = np.clip(img, 0, 255).astype(np.uint8)
+#     plt.imshow(img)
+#     plt.title("prediction: " + class_names[predict[0].item()])
+# plt.show()
+
---
+++ resnet50(save_model).py
@@ -0,0 +1,51 @@
+import torch
+import torchvision.models as models
+import torchvision.transforms as transforms
+from torchvision.datasets import CIFAR10
+from torch.utils.data import DataLoader
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+Net = models.resnet50()
+Net.to(device)
+Net.eval()
+
+transform = transforms.Compose([
+    transforms.Resize((224, 224)),
+    transforms.ToTensor(),
+    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+])
+
+train_set = CIFAR10(root="", train=True, transform=transform, download=True)
+test_set = CIFAR10(root="", train=False, transform=transform, download=True)
+train_dataloader = DataLoader(train_set, batch_size=32, shuffle=True)
+test_dataloader = DataLoader(train_set, batch_size=32, shuffle=True)
+Net.load_state_dict(torch.load('model/model30.pt'))
+
+def evaluate(test_data, net):
+    n_correct = 0
+    n_total = 0
+    with torch.no_grad():
+        for (x, y) in test_data:
+            x = x.to(device)  # 确保输入数据在正确的设备上
+            y = y.to(device)  # 确保标签数据在正确的设备上
+            outputs = net(x)
+            preds = torch.argmax(outputs, dim=1)
+            n_correct += (preds == y).sum().item()
+            n_total += y.size(0)
+    return n_correct / n_total
+
+print("initial accuracy:", evaluate(test_dataloader, Net))
+optimizer = torch.optim.Adam(Net.parameters(), lr=0.0001)
+for epoch in range(10):
+    for (x, y) in train_dataloader:
+        x = x.to(device)  # 确保输入数据在正确的设备上
+        y = y.to(device)  # 确保标签数据在正确的设备上
+        Net.zero_grad()
+        output = Net.forward(x)
+        loss = torch.nn.functional.cross_entropy(output, y)
+        loss.backward()
+        optimizer.step()
+    print("epoch", epoch, "accuracy:", evaluate(test_dataloader, Net))
+
+torch.save(Net.state_dict(), 'model/model4.pt')
